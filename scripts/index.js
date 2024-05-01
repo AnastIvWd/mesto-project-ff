214de5dada2fd53c5ee65e07b883ba74a3157114
Anastasia Ivankina <anst.iv@yandex.ru>
2024-05-01 20:43:12 +0300

    Проверка 2/5

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
-function addCard(item, deleteCard) {
+function createCard(item, deleteCard) {
   const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardImage = placesItem.querySelector('.card__image');
   const cardTitle = placesItem.querySelector('.card__title');
@@ -12,7 +12,8 @@ function addCard(item, deleteCard) {
   cardImage.setAttribute('alt', item.name);
   cardTitle.textContent = item.name;
   deleteButton.addEventListener('click', deleteCard);
-  placesList.append(placesItem);
+
+  return placesItem;
 }
 
 // @todo: Функция удаления карточки
@@ -21,6 +22,10 @@ function deleteCard(evt) {
 }
 
 // @todo: Вывести карточки на страницу
+function renderCard(item) {
+  placesList.prepend(createCard(item, deleteCard));
+}
+
 initialCards.forEach((item) => {
-  addCard(item, deleteCard);
+  renderCard(item);
 });
